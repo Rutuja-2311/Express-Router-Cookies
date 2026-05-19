a0c191bd782e708bef46de85f39e5a0f03b033d9
Rutuja Anantrao Mhaske <rutujamhaske.2311@gmail.com>
2026-05-19 13:42:59 +0530

    Implement cookie parser and new cookie routes
    Added cookie parser middleware and new routes for signed cookies and greeting.

--- server.js
+++ server.js
@@ -2,6 +2,24 @@ const express = require("express");
 const app = express();
 const users = require("./routes/user.js");
 const posts = require("./routes/post.js");
+const cookieParser = require("cookie-parser");
+
+app.use(cookieParser("secretcode"));
+
+app.get("/getsignedcookie", (req, res) => {
+  res.cookie("madeIn", "India", { signed: true });
+  res.send("signed cookie send");
+});
+
+app.get("/verify", (req, res) => {
+  console.log(req.signedCookies);
+  res.send("verified");
+});
+
+app.get("/greet", (req, res) => {
+  let { name = "anonymous" } = req.cookies;
+  res.send(`Hi, ${name}`);
+});
 
 app.get("/getcookies", (req, res) => {
   res.cookie("greet", "namaste");
@@ -10,6 +28,7 @@ app.get("/getcookies", (req, res) => {
 });
 
 app.get("/", (req, res) => {
+  console.log(req.cookies);
   res.send("Hi, I am root");
 });
 
